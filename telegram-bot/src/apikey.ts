becde8be1579861465287bbe7eaa9f8497d38ddf
Nikolai <nikolayizgarshev@yandex.ru>
2022-07-10 16:07:22 +0300

    App module

---
+++ telegram-bot/src/apikey.ts
@@ -0,0 +1,1 @@
+export const TG_TOKEN = '5418032037:AAEo2VFKCbyxcVPNFgaZrcwfwQ-ESwu4ycA';
--- telegram-bot/src/app.module.ts
+++ telegram-bot/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TelegrafModule } from 'nestjs-telegraf';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as LocalSession from 'telegraf-session-local';
+import { TG_TOKEN } from './apikey';
 
+const session = new LocalSession({database: 'session_db.json'})
 @Module({
-  imports: [],
+  imports: [
+    TelegrafModule.forRoot({
+      middlewares: [session.middleware()],
+      token: TG_TOKEN
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
